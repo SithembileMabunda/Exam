a3bbbeaa32899ea9d017b3d86b0171fd6d113e8c
Sithembile <39857574+SithembileMabunda@users.noreply.github.com>
2018-07-30 10:25:32 +0200

    Update moments.c

--- Level 04/moments.c
+++ Level 04/moments.c
@@ -1,1 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moments.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smabunda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/30 09:19:31 by smabunda          #+#    #+#             */
+/*   Updated: 2018/07/30 10:24:42 by smabunda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
+
+#define HOUR 3600
+#define MIN 60
+#define Day 86400
+#define MON 2592000
+#define SEC 1
+
+char	ft_itoa(int nbr);
+
+char	*strcat(char *s1, char *s2);
+
+char	*moments(unsigned int dur)
+{
+	char *result;
+	char string[];
+	int nbr;
+
+	if (dur >= MIN)
+	{
+		minute
+	}
+	else if (dur >= HOUR)
+	{
+		hour
+	}
+	else if (dur >= DAY)
+	{
+		days
+	}
+	else if (dur >= MON)
+	{
+		month
+	}
+	else
+		second;
+	result = ft_strcat(total, string);
+	return (result)
+}
